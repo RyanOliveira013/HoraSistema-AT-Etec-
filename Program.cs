0236adb6250ae52020332162a8adce594b750cdd
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-10-08 19:50:18 -0300

    Horario e Data do seu PC

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HoraSistema_AT_Etec_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         Console.WriteLine("--Recebendo Informaçoes do Sistema");
+
+         DateTime DataHora = 
+         DateTime.Now;
+
+         Console.WriteLine();
+
+         Console.WriteLine($"{DataHora}");
+
+
+
+        }
+    }
+}
